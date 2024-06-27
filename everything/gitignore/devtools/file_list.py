47b5150e8601c2e57dbb1a84f3f0d232a139f709
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-27 16:31:50 -0700

    updating file list, good to go!..?

--- everything/gitignore/devtools/file_list.py
+++ everything/gitignore/devtools/file_list.py
@@ -26,7 +26,7 @@ def iter(path):
 file_list = iter('everything')
 print('Indexed.')
 
-f = open('everything\main\setup\file_list.json', 'w')
+f = open(r'everything\main\setup\file_list.json', 'w')
 json.dump(file_list, f)
 f.close()
 print('Dumped.')
